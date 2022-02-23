2c89c518fd0fba1821b2777f97293a532c47d3f9
Florent Chuffart <florent.chuffart@gmail.com>
2022-02-23 15:28:41 +0100

    Adding open multiqc line

--- 01_preprocessing.sh
+++ 01_preprocessing.sh
@@ -1,101 +1,54 @@
-cd ~/projects/nme2/results/GSE101657
+cd ~/projects/aml_simon/results/rnaseq_julie_integragen
 source config
 echo $gse
 echo $project
 rsync -auvP ~/projects/${project}/results/${gse}/ cargo:~/projects/${project}/results/${gse}/
 ## data description
-echo https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=${gse}
 
-# GSM2711588  Liver_Control_Diet0
-# GSM2711589  Liver_Control_Diet1
-# GSM2711590  Liver_Control_Diet2
-# GSM2711591  Liver_Control_Diet3
-# GSM2711592  Liver_Control_Diet4
-# GSM2711593  Liver_HF_High_Fat0
-# GSM2711594  Liver_HF_High_Fat1
-# GSM2711595  Liver_HF_High_Fat2
-# GSM2711596  Liver_HF_High_Fat3
-# GSM2711597  Liver_HF_High_Fat4
-# GSM2711598  Liver_HF_High_Fat5
-# GSM2711599  Liver_KD_Ketogenic0
-# GSM2711600  Liver_KD_Ketogenic1
-# GSM2711601  Liver_KD_Ketogenic2
-# GSM2711602  Liver_KD_Ketogenic3
-# GSM2711603  Liver_KD_Ketogenic4
-# GSM2711604  Liver_KD_Ketogenic5
-# GSM2711605  Liver_KD_Ketogenic6
-# GSM2711606  Kidney_Control_Diet0
-# GSM2711607  Kidney_Control_Diet1
-# GSM2711608  Kidney_Control_Diet2
-# GSM2711609  Kidney_Control_Diet3
-# GSM2711610  Kidney_Control_Diet4
-# GSM2711611  Kidney_HF_High_Fat0
-# GSM2711612  Kidney_HF_High_Fat1
-# GSM2711613  Kidney_HF_High_Fat2
-# GSM2711614  Kidney_HF_High_Fat3
-# GSM2711615  Kidney_HF_High_Fat4
-# GSM2711616  Kidney_HF_High_Fat5
-# GSM2711617  Kidney_KD_Ketogenic0
-# GSM2711618  Kidney_KD_Ketogenic1
-# GSM2711619  Kidney_KD_Ketogenic2
-# GSM2711620  Kidney_KD_Ketogenic3
-# GSM2711621  Kidney_KD_Ketogenic4
-# GSM2711622  Kidney_KD_Ketogenic5
-# GSM2711623  Kidney_KD_Ketogenic6
+# chuffarf@f-dahu:~$ ls -lha projects/datashare_epistorage/rnaseq_julie_integragen/raw/c3po-mondet-download/PJ2110424/RAWDATA/
+# total 32G
+# drwxr-xr-x 2 chuffarf l-iab 4.0K Dec 17 07:58 .
+# drwxr-xr-x 3 chuffarf l-iab 4.0K Dec 15 12:19 ..
+# -rw-r--r-- 1 chuffarf l-iab 3.0G Dec 15 12:20 HEL_R1.fastq.gz
+# -rw-r--r-- 1 chuffarf l-iab 3.1G Dec 15 12:20 HEL_R2.fastq.gz
+# -rw-r--r-- 1 chuffarf l-iab 3.2G Dec 15 12:20 HL60_R1.fastq.gz
+# -rw-r--r-- 1 chuffarf l-iab 3.2G Dec 15 12:20 HL60_R2.fastq.gz
+# -rw-r--r-- 1 chuffarf l-iab 3.5G Dec 15 12:20 K562_R1.fastq.gz
+# -rw-r--r-- 1 chuffarf l-iab 3.6G Dec 15 12:20 K562_R2.fastq.gz
+# -rw-r--r-- 1 chuffarf l-iab 3.1G Dec 15 12:20 KG1_R1.fastq.gz
+# -rw-r--r-- 1 chuffarf l-iab 3.1G Dec 15 12:20 KG1_R2.fastq.gz
+# -rw-r--r-- 1 chuffarf l-iab 2.9G Dec 15 12:20 OCI-AML3_R1.fastq.gz
+# -rw-r--r-- 1 chuffarf l-iab 2.9G Dec 15 12:20 OCI-AML3_R2.fastq.gz
+# -rw-r--r-- 1 chuffarf l-iab  514 Dec 15 12:47 md5sum.bettik.txt
+# -rw-r--r-- 1 chuffarf l-iab  514 Dec 17 07:59 md5sum.summer.txt
+# -rw-r--r-- 1 chuffarf l-iab  504 Dec 17 07:42 md5sum.txt
 
 
-
-
-## download fastq files
-# wget https://ftp.ncbi.nlm.nih.gov/sra/reports/Metadata/SRA_Accessions.tab
-sra=`cat ~/projects/${datashare}/platforms/SRA_Accessions.tab | grep ${gse} | cut -f1 | grep SRA`
-echo $sra
-srrs=`cat ~/projects/${datashare}/platforms/SRA_Accessions.tab | grep RUN | grep ${sra}| cut -f1 | grep SRR`
-echo $srrs
-echo $srrs | wc
-
-mkdir -p ~/projects/${datashare}/${gse}/raw
+# fastq.gz files 
 cd ~/projects/${datashare}/${gse}/raw
-
-echo "checking" $srrs >> checking_srrs_report.txt
-for srr in $srrs
-do
-  FILE=${srr}_1.fastq.gz
-  if [ -f $FILE ]; then
-     echo "File $FILE exists."
-  else
-     echo "File $FILE does not exist."
-     prefetch $srr
-     vdb-validate $srr
-     status=$?
-     ## take some decision ##
-     [ $status -ne 0 ] && echo "$srr check failed" || echo "$srr ok" >> checking_srrs_report.txt
-     parallel-fastq-dump --threads 16 --tmpdir /dev/shm --gzip --split-files --outdir ./ --sra-id ${srr}
-  fi
-done
+ls -lha c3po-mondet-download/PJ2110424/RAWDATA/*.fastq.gz .
+ln -s c3po-mondet-download/PJ2110424/RAWDATA/HEL_R1.fastq.gz      HEL_R1.fastq.gz
+ln -s c3po-mondet-download/PJ2110424/RAWDATA/HEL_R2.fastq.gz      HEL_R2.fastq.gz
+ln -s c3po-mondet-download/PJ2110424/RAWDATA/HL60_R1.fastq.gz     HL6_R1.fastq.gz
+ln -s c3po-mondet-download/PJ2110424/RAWDATA/HL60_R2.fastq.gz     HL6_R2.fastq.gz
+ln -s c3po-mondet-download/PJ2110424/RAWDATA/K562_R1.fastq.gz     K56_R1.fastq.gz
+ln -s c3po-mondet-download/PJ2110424/RAWDATA/K562_R2.fastq.gz     K56_R2.fastq.gz
+ln -s c3po-mondet-download/PJ2110424/RAWDATA/KG1_R1.fastq.gz      KG1_R1.fastq.gz
+ln -s c3po-mondet-download/PJ2110424/RAWDATA/KG1_R2.fastq.gz      KG1_R2.fastq.gz
+ln -s c3po-mondet-download/PJ2110424/RAWDATA/OCI-AML3_R1.fastq.gz OCI_R1.fastq.gz
+ln -s c3po-mondet-download/PJ2110424/RAWDATA/OCI-AML3_R2.fastq.gz OCI_R2.fastq.gz
 
 # SR or PE?
 ls -lha ~/projects/${datashare}/${gse}/raw
-sequencing_read_type=SR
+sequencing_read_type=PE
 
-## metadata linking sample and raw files
-gsms=`cat ~/projects/${datashare}/platforms/SRA_Accessions.tab | grep RUN | grep ${sra} | cut -f10 | cut -f1 -d_ | uniq`
-echo $gsms
-echo $gsms | wc
 cd ~/projects/${datashare}/${gse}/
-for gsm in $gsms
-do
-  echo ${gsm}  
-  srrs=`cat ~/projects/${datashare}/platforms/SRA_Accessions.tab | grep RUN | grep ${gsm} | cut -f1 | grep SRR | sort`
-  echo ${srrs}    
-  # # PE
-  # # echo raw/`echo $srrs | sed 's/ /_1\.fastq\.gz,raw\//g'`_1.fastq.gz raw/`echo $srrs | sed 's/ /_2\.fastq\.gz,raw\//g'`_2.fastq.gz > ${gsm}_notrim_fqgz.info
-  # # SR
-  echo raw/`echo $srrs | sed 's/ /_1\.fastq\.gz,raw\//g'`_1.fastq.gz > ${gsm}_notrim_fqgz.info
-done
+echo raw/HEL_R1.fastq.gz raw/HEL_R2.fastq.gz > HEL_notrim_fqgz.info
+echo raw/HL6_R1.fastq.gz raw/HL6_R2.fastq.gz > HL6_notrim_fqgz.info
+echo raw/K56_R1.fastq.gz raw/K56_R2.fastq.gz > K56_notrim_fqgz.info
+echo raw/KG1_R1.fastq.gz raw/KG1_R2.fastq.gz > KG1_notrim_fqgz.info
+echo raw/OCI_R1.fastq.gz raw/OCI_R2.fastq.gz > OCI_notrim_fqgz.info
 cat *.info
-
-
 
 ## qc align count
 # put wf on dahu and launch
@@ -110,4 +63,4 @@ mkdir -p ~/projects/${datashare}/${gse}/raw/
 rsync -auvP dahu:~/projects/${datashare}/${gse}/*.txt ~/projects/${datashare}/${gse}/
 rsync -auvP dahu:~/projects/${datashare}/${gse}/raw/*.html ~/projects/${datashare}/${gse}/raw/
 rsync -auvP dahu:~/projects/${datashare}/${gse}/raw/multiqc_notrim* ~/projects/${datashare}/${gse}/raw/
-
+open ~/projects/${datashare}/${gse}/raw/multiqc_notrim.html
