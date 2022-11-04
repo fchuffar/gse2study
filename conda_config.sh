7e1d53c1f6d7e9634b86aaca939a8f22916276a1
Florent Chuffart <florent.chuffart@gmail.com>
2022-11-04 16:06:08 +0100

    Update conda_config.sh

--- conda_config.sh
+++ conda_config.sh
@@ -22,73 +22,22 @@ conda activate rnaseq_env
 
 conda install  -c bioconda snakemake fastqc samtools star htseq deeptools
 pip install --upgrade snakemake
-conda install -c bioconda -c conda-forge multiqc
+conda install -c bioconda -c conda-forge multiqc 
 
-# conda install  -c anaconda libopenblas
-#
-# conda install -c r \
-#   r-base=3.6.1     \
-#   r-rmarkdown      \
-#   r-devtools       \
-#   r r-xml
-#
-#   install.package("https://cran.r-project.org/src/contrib/Archive/locfit/locfit_1.5-9.tar.gz")
-#
-#
-# # conda install --no-update-deps -c r r-xml
-#
-# conda install -c bioconda \
-#   bioconductor-Biobase    \
-#   bioconductor-affy       \
-#   bioconductor-GEOquery
-#
-# # conda install --no-update-deps -c bioconda            \
-# #   bioconductor-illuminahumanmethylation27kanno.ilmn12.hg19    \
-# #   bioconductor-illuminahumanmethylation450kanno.ilmn12.hg19   \
-# #   bioconductor-illuminahumanmethylationepicanno.ilm10b4.hg19
-#
-#
-# #conda install -c r r-locfit
-# conda install -c r r-glmnet
-#
-#
-#
-# conda install -c bioconda bioconductor-rhdf5lib
-# conda install -c bioconda bioconductor-rhdf5
-# conda install -c bioconda bioconductor-hdf5array
-# conda install -c bioconda snakemake
-#
-#
-# BiocManager::install("HDF5Array")
-# BiocManager::install("minfi")
-# BiocManager::install("IlluminaHumanMethylation27kanno.ilmn12.hg19")
-# BiocManager::install("IlluminaHumanMethylation450kanno.ilmn12.hg19")
-# BiocManager::install("IlluminaHumanMethylationEPICanno.ilm10b4.hg19")
-# BiocManager::install("EpiDISH")
-#
-#
-#
-#
+conda install -c r -c bioconda \
+  r-base=3.6.1     \
+  r-rmarkdown      \
+  r-devtools       \
+  r r-xml \
+  bioconductor-Biobase    \
+  bioconductor-affy       \
+  bioconductor-GEOquery
+
+conda install -c bioconda bioconductor-deseq2
+
+install.packages("https://cran.r-project.org/src/contrib/Archive/locfit/locfit_1.5-9.tar.gz")
+
+
 # # https://github.com/fchuffar/epimedtools
 # Sys.setenv(TAR = "/bin/tar") ; devtools::install_github("fchuffar/epimedtools")
-# Sys.setenv(TAR = "/bin/tar") ; devtools::install_github("fchuffar/dnamaging")
 
-# BiocManager::install("impute")
-
-
-# install.packages("dbplyr")
-# install.packages("tidyr")
-# install.packages("memoise")
-# install.packages("glmnet")
-# install.packages("glmnetUtils")
-# install.packages("beeswarm")
-#
-#
-#
-#
-# # bioconductor-methylclockdata                                \
-# # bioconductor-methylclock                                    \
-#
-#
-#
-#
