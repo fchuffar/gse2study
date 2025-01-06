8c2a22e65f505e0cac6bc9725c29c3293835e479
Florent Chuffart <florent.chuffart@gmail.com>
2025-01-06 10:10:41 +0100

    Update conda_config.sh

--- conda_config.sh
+++ conda_config.sh
@@ -1,6 +1,6 @@
 conda create -n rnaseq_env
 conda activate rnaseq_env
-mamba install -c anaconda -c bioconda -c conda-forge -c r r-base=4.2.0 libopenblas bioconductor-geoquery bioconductor-affy bioconductor-biobase r-seqinr r-rcpparmadillo r-devtools r-fastmap r-matrix r-kernsmooth r-catools r-gtools r-nortest r-survival r-beanplot r-gplots \
+mamba install -c anaconda -c bioconda -c conda-forge -c r r-base libopenblas bioconductor-geoquery bioconductor-affy bioconductor-biobase r-seqinr r-rcpparmadillo r-devtools r-fastmap r-matrix r-kernsmooth r-catools r-gtools r-nortest r-survival r-beanplot r-gplots \
   r-sartools
 # under R
 # devtools::install_github("fchuffar/epimedtools")
