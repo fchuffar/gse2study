d036b810615d0826139175c0ba418b511f0bc6b9
Florent Chuffart <florent.chuffart@gmail.com>
2025-05-13 22:01:46 +0200

    Update conda_config.sh

--- conda_config.sh
+++ conda_config.sh
@@ -1,6 +1,6 @@
 conda create -n rnaseq_env
 conda activate rnaseq_env
 mamba install -c anaconda -c bioconda -c conda-forge -c r r-base libopenblas bioconductor-geoquery bioconductor-affy bioconductor-biobase r-seqinr r-rcpparmadillo r-devtools r-fastmap r-matrix r-kernsmooth r-catools r-gtools r-nortest r-survival r-beanplot r-gplots \
-  r-sartools r-openxlsx
+  r-sartools r-openxlsx  snakemake=7.32.4 python=3.9
 # under R
 # devtools::install_github("fchuffar/epimedtools")
