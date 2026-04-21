534e426a13a4195f22a9f30c973f9f0b88d51f04
Florent Chuffart <florent.chuffart@gmail.com>
2026-04-21 16:31:36 +0200

    Adding edger support

--- 04_edger/da_report.R
+++ 04_edger/da_report.R
@@ -22,7 +22,7 @@ targetFile <- "design.txt"                           # path to the design/target
 write.table(
   cbind(data.frame(
           label = design$gsm,
-          files = paste0(design$gsm, "_notrim_star_", species, "_", annotation, "_", genome_version , "_", gtf_prefix, "_stranded", strand, "_classiccounts.txt"),
+          files = paste0(design$gsm, "_notrim_star_", species, "_", annotation, "_", version , "_", gtf_prefix, "_stranded", strand, "_classiccounts.txt"),
           stringsAsFactors=FALSE
         ), design), 
   targetFile,sep=" ", quote=FALSE, row.names=FALSE
@@ -38,37 +38,34 @@ varInt <- "cond"                                     # factor of interest
 condRef <- as.character(levels(design[,varInt])[1])                                       # reference biological condition
 batch <- NULL                                        # blocking factor: NULL (default) or "batch" for example
 
-fitType <- "parametric"                              # mean-variance relationship: "parametric" (default) or "local"
-cooksCutoff <- TRUE                                  # TRUE/FALSE to perform the outliers detection (default is TRUE)
-independentFiltering <- TRUE                         # TRUE/FALSE to perform independent filtering (default is TRUE)
 alpha <- 0.05                                        # threshold of statistical significance
 pAdjustMethod <- "BH"                                # p-value adjustment method: "BH" (default) or "BY"
 
-typeTrans <- "VST"                                   # transformation for PCA/clustering: "VST" or "rlog"
-locfunc <- "median"                                  # "median" (default) or "shorth" to estimate the size factors
+cpmCutoff <- 1                                       # counts-per-million cut-off to filter low counts
+gene.selection <- "pairwise"                         # selection of the features in MDSPlot
+normalizationMethod <- "TMM"                         # normalization method: "TMM" (default), "RLE" (DESeq) or "upperquartile"
 
 colors <- c("#f3c300", "#875692", "#f38400",         # vector of colors of each biological condition on the plots
             "#a1caf1", "#be0032", "#c2b280",
             "#848482", "#008856", "#e68fac",
             "#0067a5", "#f99379", "#604e97")
 
-            
-
+forceCairoGraph <- FALSE
 ################################################################################
 ###                             running script                               ###
 ################################################################################
 setwd(workDir)
 library(SARTools)
+if (forceCairoGraph) options(bitmapType="cairo")
 
 # checking parameters
-checkParameters.DESeq2(projectName=projectName,author=author,targetFile=targetFile,
-                       rawDir=rawDir,featuresToRemove=featuresToRemove,varInt=varInt,
-                       condRef=condRef,batch=batch,fitType=fitType,cooksCutoff=cooksCutoff,
-                       independentFiltering=independentFiltering,alpha=alpha,pAdjustMethod=pAdjustMethod,
-                       typeTrans=typeTrans,locfunc=locfunc,colors=colors)
+checkParameters.edgeR(projectName=projectName,author=author,targetFile=targetFile,
+                      rawDir=rawDir,featuresToRemove=featuresToRemove,varInt=varInt,
+                      condRef=condRef,batch=batch,alpha=alpha,pAdjustMethod=pAdjustMethod,
+                      cpmCutoff=cpmCutoff,gene.selection=gene.selection,
+                      normalizationMethod=normalizationMethod,colors=colors)
 
 # loading target file
-
 loadTargetFile = function (targetFile, varInt, condRef, batch) 
 {
     target <- read.table(targetFile, header = TRUE, sep = " ")
@@ -90,7 +87,6 @@ loadTargetFile = function (targetFile, varInt, condRef, batch)
     print(target)
     return(target)
 }
-
 target <- loadTargetFile(targetFile=targetFile, varInt=varInt, condRef=condRef, batch=batch)
 
 # loading counts
@@ -99,29 +95,23 @@ counts <- loadCountData(target=target, rawDir=rawDir, featuresToRemove=featuresT
 # description plots
 majSequences <- descriptionPlots(counts=counts, group=target[,varInt], col=colors)
 
-# analysis with DESeq2
-out.DESeq2 <- run.DESeq2(counts=counts, target=target, varInt=varInt, batch=batch,
-                         locfunc=locfunc, fitType=fitType, pAdjustMethod=pAdjustMethod,
-                         cooksCutoff=cooksCutoff, independentFiltering=independentFiltering, alpha=alpha)
+# edgeR analysis
+out.edgeR <- run.edgeR(counts=counts, target=target, varInt=varInt, condRef=condRef,
+                       batch=batch, cpmCutoff=cpmCutoff, normalizationMethod=normalizationMethod,
+                       pAdjustMethod=pAdjustMethod)
 
-# PCA + clustering
-exploreCounts(object=out.DESeq2$dds, group=target[,varInt], typeTrans=typeTrans, col=colors)
+# MDS + clustering
+exploreCounts(object=out.edgeR$dge, group=target[,varInt], gene.selection=gene.selection, col=colors)
 
-# summary of the analysis (boxplots, dispersions, diag size factors, export table, nDiffTotal, histograms, MA plot)
-summaryResults <- summarizeResults.DESeq2(out.DESeq2, group=target[,varInt], col=colors,
-                                          independentFiltering=independentFiltering,
-                                          cooksCutoff=cooksCutoff, alpha=alpha)
+# summary of the analysis (boxplots, dispersions, export table, nDiffTotal, histograms, MA plot)
+summaryResults <- summarizeResults.edgeR(out.edgeR, group=target[,varInt], counts=counts, alpha=alpha, col=colors)
 
 # save image of the R session
 save.image(file=paste0(projectName, ".RData"))
 
 # generating HTML report
-writeReport.DESeq2(target=target, counts=counts, out.DESeq2=out.DESeq2, summaryResults=summaryResults,
-                   majSequences=majSequences, workDir=workDir, projectName=projectName, author=author,
-                   targetFile=targetFile, rawDir=rawDir, featuresToRemove=featuresToRemove, varInt=varInt,
-                   condRef=condRef, batch=batch, fitType=fitType, cooksCutoff=cooksCutoff,
-                   independentFiltering=independentFiltering, alpha=alpha, pAdjustMethod=pAdjustMethod,
-                   typeTrans=typeTrans, locfunc=locfunc, colors=colors)
-
-rmarkdown::render("volcano.Rmd")
-
+writeReport.edgeR(target=target, counts=counts, out.edgeR=out.edgeR, summaryResults=summaryResults,
+                  majSequences=majSequences, workDir=workDir, projectName=projectName, author=author,
+                  targetFile=targetFile, rawDir=rawDir, featuresToRemove=featuresToRemove, varInt=varInt,
+                  condRef=condRef, batch=batch, alpha=alpha, pAdjustMethod=pAdjustMethod, cpmCutoff=cpmCutoff,
+                  colors=colors, gene.selection=gene.selection, normalizationMethod=normalizationMethod)
