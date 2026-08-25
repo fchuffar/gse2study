a20b614ecff4566bb918787fee4acac07b05ea52
Florent Chuffart <florent.chuffart@gmail.com>
2026-08-25 16:25:47 -0400

    Update geo_submission.sh

--- geo_submission/geo_submission.sh
+++ geo_submission/geo_submission.sh
@@ -1,37 +1,48 @@
-# source ../01_preprocessing.sh first lines...
+cd ~/projects/lfs_patients_fibroblast/results/rnaseq_lfs2_icm
+source config
+echo $gse
+echo $project
+
 ssh cargo
 
-# Transfering files
-cd ~/projects/datashare/${gse}/
-ls -lha raw/*1.fastq.gz raw/*2.fastq.gz
-cat raw/md5.bettik.txt 
-ls -lha *_${gtf_prefix}_stranded${strand}_classiccounts.txt
+## Transfering files
 mkdir -p /bettik/chuffarf/geo_submission/${project}/${gse}/fastq
 mkdir -p /bettik/chuffarf/geo_submission/${project}/${gse}/counts
+cd ~/projects/datashare/${gse}/
+# counts
+ls -lha *_${gtf_prefix}_stranded${strand}_classiccounts.txt
+rsync -auvP --copy-links \
+  ~/projects/datashare/${gse}/*_${gtf_prefix}_stranded${strand}_classiccounts.txt \
+  /bettik/chuffarf/geo_submission/${project}/${gse}/counts
+# fastq
+ls -lha raw/*1.fastq.gz raw/*2.fastq.gz
 rsync -auvP --copy-links \
   ~/projects/datashare/${gse}/raw/*1.fastq.gz \
   ~/projects/datashare/${gse}/raw/*2.fastq.gz \
   /bettik/chuffarf/geo_submission/${project}/${gse}/fastq/.
-rsync -auvP --copy-links \
-  ~/projects/datashare/${gse}/*_${gtf_prefix}_stranded${strand}_classiccounts.txt \
-  /bettik/chuffarf/geo_submission/${project}/${gse}/counts
-
-ls -lha /bettik/chuffarf/geo_submission/${project}/${gse}/*
+# check
+ls -lha /bettik/chuffarf/geo_submission/${project}/
+ls -lha /bettik/chuffarf/geo_submission/${project}/${gse}
 ls -lha /bettik/chuffarf/geo_submission/${project}/${gse}/fastq/
 ls -lha /bettik/chuffarf/geo_submission/${project}/${gse}/counts/
 
 # MD5
+MD5BETTIKFQFILE=~/projects/datashare/${gse}/raw/md5.bettik.txt
+ls ${MD5BETTIKFQFILE}
+MD5GEOFQFILE=/bettik/chuffarf/geo_submission/${project}/md5.${gse}.fq.geo.txt
+MD5GEOcntFILE=/bettik/chuffarf/geo_submission/${project}/md5.${gse}.cnt.geo.txt
 cd /bettik/chuffarf/geo_submission/${project}/${gse}/fastq
-md5sum *.fastq.gz > md5.geo.txt
-cat md5.geo.txt | cut -f1 -d" " | sort > /tmp/tmp.md5.geo.txt
-cat ~/projects/datashare/${gse}/raw/md5.bettik.txt  | cut -f1 -d" " | sort > /tmp/tmp.md5.bettik.txt
-diff /tmp/tmp.md5.geo.txt /tmp/tmp.md5.bettik.txt
+md5sum *.fastq.gz > ${MD5GEOFQFILE}
+cat ${MD5BETTIKFQFILE} | cut -f1 -d" " | sort > /tmp/tmp.${gse}.fq.md5.bettik.txt
+cat ${MD5GEOFQFILE} | cut -f1 -d" " | sort > /tmp/tmp.${gse}.fq.md5.geo.txt
+diff /tmp/tmp.${gse}.fq.md5.geo.txt /tmp/tmp.${gse}.fq.md5.bettik.txt
 cd /bettik/chuffarf/geo_submission/${project}/${gse}/counts
-md5sum *.txt > md5.geo.txt
+md5sum *.txt > ${MD5GEOcntFILE}
 
-ls -lha /bettik/chuffarf/geo_submission/${project}/${gse}/*
+ls -lha /bettik/chuffarf/geo_submission/${project}/
+ls -lha /bettik/chuffarf/geo_submission/${project}/${gse}/
 ls -lha /bettik/chuffarf/geo_submission/${project}/${gse}/fastq/
-ls -lha /bettik/chuffarf/geo_submission/${project}/${gse}/counts/
+
 
 # Put metadata
 source ~/conda_config.sh 
@@ -45,7 +56,9 @@ rsync -auvP cargo:~/projects/${project}/results/${gse}/geo_submission/0*_*.xlsx 
 # localy 
 cd ~/projects/${project}/results/${gse}/geo_submission
 # wget https://www.ncbi.nlm.nih.gov/geo/info/examples/seq_template.xlsx
+# Afin que de proceder au dépôt des données et obtenir le numéro d'accession (GSE) ainsi que le code d'accès destiné aux reviewers, il faut completer, dans le fichier seq_template.xlsx, les sections STUDY et PROTOCOLS.
 open seq_template.xlsx
+
 
 rsync -auvP seq_template.xlsx cargo:/bettik/chuffarf/geo_submission/${project}/${gse}/.
 
